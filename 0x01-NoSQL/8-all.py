e54803e63c1df70c90b13fde08b5d74d4bb8003e
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-07-15 19:03:18 +0100

    refactor: Add function to list all documents in a collection

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" This module contains a function to list all the docs in a collection"""
+
+
+def list_all(mongo_collection) -> list:
+    """_summary_
+
+    Args:
+        mongo_collection: This a mongo collection
+
+    Returns:
+        list: Returns a list of documents in the mongo_collection
+    """
+    if mongo_collection is None:
+        return []
+    return mongo_collection.find()
