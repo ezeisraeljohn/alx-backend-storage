6274380e5d54856c141b9cc3760843f2bb9785d4
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-07-17 21:36:26 +0100

    feat: Add Cache class for storing cached data

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+""" This contains a redis class known as Cache"""
+
+
+import redis
+from typing import Union
+import uuid
+
+
+class Cache:
+    """The Cache class to store all cached data"""
+
+    def __init__(self) -> None:
+        """The Init Method"""
+        self.__redis = redis.Redis()
+        self.__redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """Returns a the key of a stored value
+
+        Args:
+            data (Union[str, bytes, int, float]): The data to store in the db
+
+        Returns:
+            str: Returns the key representing the value stored in the db
+        """
+        key = uuid.uuid4()
+        self.__redis.set(str(key), data)
+        return str(key)
