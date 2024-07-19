8a39aa25a685d27abb2f695ab94d591340719f77
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-07-19 22:09:04 +0100

    refactor: Update get_page function to use string formatting for Redis key

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,8 +2,22 @@
 """ The Module that caches a website's url"""
 import requests
 import redis
+from typing import Callable, Any
+import functools
 
 
+def my_decorator(method: Callable) -> Any:
+    @functools.wraps(method)
+    def wrapper(url):
+        redis_instance = redis.Redis()
+        redis_instance.incr("count:{}".format(url), 1)
+        redis_instance.expire("count:{}".format(url), 10)
+        return method(url)
+
+    return wrapper
+
+
+@my_decorator
 def get_page(url: str) -> str:
     """This will return the html of a url and cache the url
 
@@ -13,7 +27,4 @@ def get_page(url: str) -> str:
         str: The return value
     """
     response = requests.get(url=url)
-    redis_instance = redis.Redis()
-    redis_instance.incr("count:{}".format(url), 1)
-    redis_instance.expire("count:{}".format(url), 10)
     return response.text
